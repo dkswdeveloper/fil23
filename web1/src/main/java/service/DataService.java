0e48ca3787b9629ff469819c498242e55c5719db
Dinesh Kumar (C) <dinesh_budhiraja@yahoo.com>
2023-10-17 17:05:50 +0530

    deleted all files
    enter the commit message for your changes. Lines starting

--- web1/src/main/java/service/DataService.java
+++
@@ -1,7 +0,0 @@
-package service;
-
-import java.util.List;
-
-public interface DataService {
-	List<String> getData(String username);
-}
--- web1/src/main/java/service/LoginService.java
+++
@@ -1,5 +0,0 @@
-package service;
-
-public interface LoginService {
-	public boolean checkLogin(String username, String password);
-}
